aa541be45ee8a81636396a83d5b1eaec38f8910d
Ege Gürsel <gurege.24@robcol.k12.tr>
2022-01-23 01:51:45 +0300

    a

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -5,9 +5,4 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
-
-    public void TriggerDialogue()
-    {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
-    }
 }
